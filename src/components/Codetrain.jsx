888fd55475b9fd68e994db9561a57a18d2f2efa4
EkowPatrick <ekowpatrick@yahoo.com>
2021-01-30 14:23:10 +0000

    ReactProject1

---
+++ src/components/Codetrain.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Codetrain extends React.Component{
+    render(){
+        return <h1>Codetrain is awesome, react works!!!!</h1>
+    }
+}
+
+export default Codetrain;
